dfff25592f7dbfb8d25782b45fefa3b869d7e9d1
ImWalker <myfirstfakeid2013@yahoo.com>
2021-10-25 19:54:15 +0300

    Updates

--- src/containers/scroll.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Scroll =(props) =>{
-	return (
-		<div style={{overflowY:'scroll', border: '1px', height:'750px'}}>
-		{props.children}
-		</div>
-		);
-};
-export default Scroll;
